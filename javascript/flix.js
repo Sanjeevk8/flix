7c48d1dce4b5134ac0a2323d8ed19db2022ac79b
Sanjeev <kumarsanjeev0048@gmail.com>
2023-08-21 20:50:36 +0530

    commits

---
+++ javascript/flix.js
@@ -0,0 +1,28 @@
+//for search bar header
+$('.img').click(function() { 
+    $('.input').toggleClass('toggle')
+  });
+  
+  $(document).click(function(e) {
+    if (!$(e.target).parent(".search").length) {
+      $(".search").find(".input").removeClass("toggle");
+    }
+  });
+
+//image of series set
+
+  const centerdiv=document.getElementById("centerdiv");
+
+  fetch("netflix.json.json").then((apidata)=>{
+    return apidata.json();
+  })
+  .then((jsondata)=>{
+    jsondata.map((val)=>{
+      key = val.id;
+      let img = document.createElement("img");
+      img.src = val._embedded.show.image.medium;
+      centerdiv.appendChild(img);
+      // console.log(val._embedded.show.image.medium);
+    });
+  });
+
