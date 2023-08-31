6b4fbb18c6d463765d67ebf0e8ecd5f58a9ab6c0
Sanjeev <kumarsanjeev0048@gmail.com>
2023-08-31 22:50:01 +0530

    img set

--- javascript/flix.js
+++ javascript/flix.js
@@ -20,10 +20,27 @@ $('.img').click(function() {
     jsondata.map((val)=>{
       key = val.id;
       let img = document.createElement("img");
-      img.src = val._embedded.show.image.medium;
+      img.classList.add("img_poster");
+      img.src = val._embedded.show.image.original;
       centerdiv.appendChild(img);
-      console.log(val._embedded.show.image.medium);
-      
-    });
+      // console.log(val._embedded.show.image.medium);
+      });
+  });
+
+  const centerdiv1=document.getElementById("centerdiv1");
+
+
+  fetch("netflix.json").then((apidata)=>{
+    return apidata.json();
+  })
+  .then((jsondata)=>{
+    jsondata.map((value)=>{
+      key = value.id;
+      let img1 = document.createElement("img");
+      img1.classList.add("img_poster");
+      img1.src = value._embedded.show.image.medium;
+      centerdiv1.appendChild(img1);
+      console.log(value._embedded.show.image.medium);
+      });
   });
 
